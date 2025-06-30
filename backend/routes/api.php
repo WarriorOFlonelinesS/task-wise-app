e48fa839bed406228e486b2bc17545931de8f7d8
WarriorOFlonelinesS <ydneprovskiy@gmail.com>
2025-06-30 15:37:58 +0200

    fix: fix error504

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ping', function () {
+    return 'pong';
+});
+
 Route::get('/', function () {
     return response()->json([
         'message' => 'Welcome to the TaskWise API!',
